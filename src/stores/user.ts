e3f0f5532f60afa7c8392a73bf96cfbb25b7e148
汝震 郭 <375046751@qq.com>
2025-06-03 19:34:32 +0800

    实现了管理员登录和用户登录不同的跳转，实现了文章详细内容页面-2

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -12,27 +12,6 @@ export const userInfoStore=defineStore('userInfo',{
             userPhone:null,
         }
     }),
-    actions: {
-        // 设置登录用户信息
-        setUserInfo(userData:any) {
-            this.userInfo.userId = userData.userId;
-            this.userInfo.user_name = userData.user_name;
-            this.userInfo.userAuthority = userData.userAuthority;
-            this.userInfo.userPhone = userData.userPhone;
-            this.isLogin = true;
-        },
-
-        clearUserInfo() {
-            this.userInfo.userId = null;
-            this.userInfo.user_name = null;
-            this.userInfo.userAuthority = null;
-            this.userInfo.userPhone = null;
-            this.isLogin = false;
-        },
-        setLoginStatus(status:any) {
-            this.isLogin = status;
-        }
-    },
     persist:{
         storage:localStorage,//读取时采用sessionStorage.getItem("key")
     },
